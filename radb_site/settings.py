4a93d9a7d87b4670ba3405a80d82636e52a112fc
Nikola Simic <simic.ra32.2020@uns.ac.rs>
2022-06-23 13:01:01 +0200

    staticfiles fix

--- radb_site/settings.py
+++ radb_site/settings.py
@@ -134,7 +134,7 @@ EMAIL_PORT = 587
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_staticfiles_')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '_staticfiles_')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'radb_site/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
