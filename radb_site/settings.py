a25e99c9df7949c6bcef6182f917f5e06b99d539
Nikola Simic <simic.ra32.2020@uns.ac.rs>
2022-06-23 12:55:37 +0200

    ...

--- radb_site/settings.py
+++ radb_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-zp=3d+n%e(es82z!+q7q7o_fzfcl)j85*rl$@#2@@%c_+j8)_5' #os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
